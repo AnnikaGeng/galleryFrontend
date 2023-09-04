b0c4377276f044b3c33acfa107da0ee651867e55
AnnikaGeng <annika.geng@foxmail.com>
2023-09-04 19:54:43 +0100

    improvement

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -1,46 +1,70 @@
-import React, {useState, useEffect} from 'react'
-import Star from './Star.jsx'
-import {BASE_API_URL} from '../config/config.js'
+import React, { useState, useEffect } from "react";
+import Star from "./Star.jsx";
+import { BASE_API_URL } from "../config/config.js";
 import { motion } from "framer-motion";
 
 const Gallery = () => {
-  const [data, setData] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [page, setPage] = useState(0);
+  const [endOfPage, setEndOfPage] = useState(false);
 
-  const getAllImages = async () => {
-    setIsLoading(true)
+  const loadMoreData = async () => {
+    setIsLoading(true);
+
     try {
-        const response = await fetch(`${BASE_API_URL}/`);
+      // use cache first
+      const cachedData = localStorage.getItem(`page${page + 1}`);
+
+      if (cachedData) {
+        const parsedData = JSON.parse(cachedData);
+        if (parsedData.length < 10) {
+          setEndOfPage(true);
+        }
+        setData([...data, ...parsedData]);
+        setPage(page + 1);
+        setIsLoading(false);
+      } else {
+        // Fetch the data from the API
+        const response = await fetch(`${BASE_API_URL}/?page=${page + 1}`);
         const responseJson = await response.json();
-        setData(responseJson);
+        if (responseJson.length < 10) {
+          setEndOfPage(true);
+        }
+
+        // Save the new data in localStorage for the next page
+        localStorage.setItem(`page${page + 1}`, JSON.stringify(responseJson));
+
+        setData([...data, ...responseJson]);
+        setPage(page + 1);
         setIsLoading(false);
+      }
     } catch (error) {
-        console.error("Error while fetching the image. Try again later.");
-        setIsLoading(false);
+      console.error("Error while fetching the image. Try again later.");
+      setIsLoading(false);
     }
   };
 
   function getRandomSize() {
-    const sizes = ['small', 'medium', 'large'];
+    const sizes = ["small", "medium", "large"];
     const randomIndex = Math.floor(Math.random() * sizes.length);
     return sizes[randomIndex];
   }
   const containerVariants = {
     hidden: {},
     visible: {
-      transition: { staggerChildren: 0.1 }, // Adjust the stagger duration as needed
+      transition: { staggerChildren: 0.1 },
     },
   };
 
-
   useEffect(() => {
-    getAllImages();
-}, [data.length]);
+    loadMoreData();
+  }, []);
 
   return (
     <div>
-        {isLoading ? null : (
-          <motion.div
+      {isLoading ? null : (
+        <motion.div
           initial="hidden"
           animate="visible"
           variants={containerVariants}
@@ -53,11 +77,12 @@ const Gallery = () => {
             variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
-          }}
+            }}
           >
-            <h1 className=' text-white m-10 text-4xl lg:text-left lg:ml-36 text-center'>My Gallery</h1>
+            <h1 className=" text-white m-10 text-4xl lg:text-left lg:ml-36 text-center">
+              My Gallery
+            </h1>
           </motion.div>
-                
           <motion.div
             initial="hidden"
             whileInView="visible"
@@ -66,39 +91,45 @@ const Gallery = () => {
             variants={{
               hidden: { opacity: 0, x: -50 },
               visible: { opacity: 1, x: 0 },
-          }}
+            }}
             style={styles.container}
           >
-          {data?.sort((a, b) => b.id - a.id).map((star, index) => (
-              <Star 
-              key={star.id}
-              data={star} 
-              index={index} 
-              size={getRandomSize()}
+            {data.map((star, index) => (
+              <Star
+                key={star.id}
+                data={star}
+                index={index}
+                size={getRandomSize()}
               />
-          ))}
+            ))}
           </motion.div>
-            </motion.div>
-        )}
-
+          <p
+            onClick={endOfPage ? null : loadMoreData}
+            className={`text-white ${endOfPage ? "" : "cursor-pointer"} pt-10`}
+          >
+            {endOfPage ? "No more data" : "Load more data"}
+          </p>
+          )
+        </motion.div>
+      )}
     </div>
-  )
-}
+  );
+};
 
 const styles = {
   container: {
     margin: 0,
     padding: 0,
-    width: '100vw',
-    backgroundColor: '#0B1320',
-    position: 'relative',
-    left: '0%',
-    transform: 'translateX(-50%)',
-    display: 'grid',
-    gridTemplateColumns: 'repeat(auto-fill, 400px)',
-    gridAutoRows: '20px',
-    justifyContent: 'center',
-  }
-}
+    width: "100vw",
+    backgroundColor: "#0B1320",
+    position: "relative",
+    left: "0%",
+    transform: "translateX(-50%)",
+    display: "grid",
+    gridTemplateColumns: "repeat(auto-fill, 400px)",
+    gridAutoRows: "20px",
+    justifyContent: "center",
+  },
+};
 
-export default Gallery
+export default Gallery;
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,123 +1,138 @@
-import React, {useEffect, useState} from 'react'
-import {BASE_API_URL} from '../config/config.js'
+import React, { useEffect, useState } from "react";
+import { BASE_API_URL } from "../config/config.js";
 import { toast } from "react-toastify";
-import axios from 'axios'
-import "../App.css"
+import axios from "axios";
+import "../App.css";
 import { motion } from "framer-motion";
-import { LazyLoadImage } from 'react-lazy-load-image-component';
-import 'react-lazy-load-image-component/src/effects/blur.css';
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import "react-lazy-load-image-component/src/effects/blur.css";
 
 const Main = () => {
-    const [data, setData] = useState()
-    const [isLoading, setIsLoading] = useState(false)
+  const [data, setData] = useState();
+  const [isLoading, setIsLoading] = useState(false);
 
-    const getTodaysImage = async () => {
-        setIsLoading(true)
-        try {
-            const response = await fetch(`${BASE_API_URL}/today`)
-            const responseJson = await response.json();
-            setData(responseJson.information);
-            setIsLoading(false)
-        } catch (error) {
-            toast.error("Error while fetching the image. Try again later.")
-            setIsLoading(false)
-        }
+  const getTodaysImage = async () => {
+    setIsLoading(true);
+
+    try {
+      const cachedData = localStorage.getItem("todaysImage");
+      const cachedTimestamp = localStorage.getItem("todaysImageTimestamp");
+
+      const currentDate = new Date().toDateString();
+      //  check if the image is valid for today
+      if (cachedData && cachedTimestamp === currentDate) {
+        const parsedData = JSON.parse(cachedData);
+        setData(parsedData);
+        setIsLoading(false);
+      } else {
+        const response = await fetch(`${BASE_API_URL}/today`);
+        const responseJson = await response.json();
+
+        localStorage.setItem(
+          "todaysImage",
+          JSON.stringify(responseJson.information)
+        );
+        localStorage.setItem("todaysImageTimestamp", currentDate);
+
+        setData(responseJson.information);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      toast.error("Error while fetching the image. Try again later.");
+      setIsLoading(false);
     }
+  };
 
-    useEffect(() => {
-        getTodaysImage()
-    }, [])
+  useEffect(() => {
+    getTodaysImage();
+  }, []);
 
-    const containerVariants = {
-        hidden: {},
-        visible: {
-          transition: { staggerChildren: 0.1 }, // Adjust the stagger duration as needed
-        },
-      };
+  const containerVariants = {
+    hidden: {},
+    visible: {
+      transition: { staggerChildren: 0.1 }, // Adjust the stagger duration as needed
+    },
+  };
 
-    const downloadImage = async (e) => {
-        console.log("Download button clicked");
-        try {
-            const response = await axios.post(`${BASE_API_URL}/download/today`);
-            toast.success("Image downloaded to gallery.");
-            setTimeout(() => {
-                window.location.reload();
-            }, 3000);
-        } catch (error) {
-            console.error("Error while downloading the image:", error);
-            toast.error("Error while downloading the image. Try again later.");
-        }
-    };
-        
+  const downloadImage = async (e) => {
+    console.log("Download button clicked");
+    try {
+      const response = await axios.post(`${BASE_API_URL}/download/today`);
+      toast.success("Image downloaded to gallery.");
+      setTimeout(() => {
+        window.location.reload();
+      }, 3000);
+    } catch (error) {
+      console.error("Error while downloading the image:", error);
+      toast.error("Error while downloading the image. Try again later.");
+    }
+  };
 
-    return (
-        <div>
-            <div>
-                {isLoading ? (
-                    <div className='flex flex-col min-h-screen'>
-                        <div className="placeholder"></div>
-                    </div>
-                ) : (
-                    <motion.div
-                    className='flex lg:flex-row flex-col'
-                    initial="hidden"
-                    animate="visible"
-                    variants={containerVariants}
-                    >
-                        {/* <motion.div
-                            className='w-screen lg:w-[75%] lg:max-h-[100vh] object-contain object-fit relative'
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                            transition={{ duration: 0.5 }}
-                            variants={{
-                            hidden: { opacity: 0, x: -50 },
-                            visible: { opacity: 1, x: 0 },
-                        }}
-                        > */}
-                        <div className='w-screen lg:w-[75%] lg:max-h-[100vh] object-contain object-fit relative'>
-                            
-                            <div className=' text-white absolute bottom-5 text-xl 
+  return (
+    <div>
+      <div>
+        {isLoading ? (
+          <div className="flex flex-col min-h-screen">
+            <div className="placeholder"></div>
+          </div>
+        ) : (
+          <motion.div
+            className="flex lg:flex-row flex-col"
+            initial="hidden"
+            animate="visible"
+            variants={containerVariants}
+          >
+            <div className="w-screen lg:w-[75%] lg:max-h-[100vh] object-contain object-fit relative">
+              <div
+                className=" text-white absolute bottom-5 text-xl 
                             left-1/2 translate-x-[-50%] 
                             md:left-1/2 md:translate-x-[-50%]
                             lg:left-10 lg:translate-x-0
                             z-10
-                            '>
-                            Astronomy Picture of Today</div>
+                            "
+              >
+                Astronomy Picture of Today
+              </div>
 
-                            <LazyLoadImage effect='blur' src={data?.url} alt={data?.title} className="w-screen lg:h-screen z-0" />
-                        {/* </motion.div> */}
-                        </div>
-                        
-                        <motion.div
-                            className='text-white text-left lg:w-[25%] p-5 w-screen flex flex-col max-h-screen
-                            justify-center content-center
-                            lg:justify-start lg:content-start'
-                            initial="hidden"
-                            whileInView="visible"
-                            viewport={{ once: true, amount: 0.5 }}
-                            transition={{ duration: 0.5 }}
-                            variants={{
-                            hidden: { opacity: 0, x: 50 },
-                            visible: { opacity: 1, x: 0 },
-                        }}
-                        >
-                        
-                            <h1 className="text-4xl font-bold">{data?.title}</h1>
-                            <p className="mt-2">{data?.date}</p>
-                            <p className="mt-4 lg:max-h-[60vh] lg:overflow-y-auto">{data?.message}</p>
-                            
-                            <button className="mt-4 bg-white text-black p-2 rounded-md hover:bg-gray-200 hover:border-none border-none"
-                                onClick={downloadImage}>Download Image
-                            </button>
-
-                        </motion.div>
-
-                    </motion.div>
-                )}
+              <LazyLoadImage
+                effect="blur"
+                src={data?.url}
+                alt={data?.title}
+                className="w-screen lg:h-screen z-0"
+              />
             </div>
-        </div>  
-      );
-}
 
-export default Main
+            <motion.div
+              className="text-white text-left lg:w-[25%] p-5 w-screen flex flex-col max-h-screen
+                            justify-center content-center
+                            lg:justify-start lg:content-start"
+              initial="hidden"
+              whileInView="visible"
+              viewport={{ once: true, amount: 0.5 }}
+              transition={{ duration: 0.5 }}
+              variants={{
+                hidden: { opacity: 0, x: 50 },
+                visible: { opacity: 1, x: 0 },
+              }}
+            >
+              <h1 className="text-4xl font-bold">{data?.title}</h1>
+              <p className="mt-2">{data?.date}</p>
+              <p className="mt-4 lg:max-h-[60vh] lg:overflow-y-auto">
+                {data?.message}
+              </p>
+
+              <button
+                className="mt-4 bg-white text-black p-2 rounded-md hover:bg-gray-200 hover:border-none border-none"
+                onClick={downloadImage}
+              >
+                Download Image
+              </button>
+            </motion.div>
+          </motion.div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Main;
